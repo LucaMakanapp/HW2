92608c692bd66279d1f180f75ded15e9f146bb30
LucaMakanapp <36860128+LucaMakanapp@users.noreply.github.com>
2021-09-14 16:17:13 -0400

    Update hawkins-r:makarushkaNapp-l-hw1.rkt

--- hawkins-r:makarushkaNapp-l-hw1.rkt
+++ hawkins-r:makarushkaNapp-l-hw1.rkt
@@ -147,10 +147,10 @@
 ;; numeric-strings : ListOfString -> ListOfString
 ;  consumes a ListOfString and produces a ListOfString that contains only those strings from the original list that consist entirely of numeric characters
 
-;(numeric-strings (cons "123" (cons "abc" (cons "456" (cons "789" empty)))))
+
+;(check-expect (numeric-strings (cons "123" (cons "abc" (cons "456" (cons "789" empty)))))(numeric-strings (cons "123" (cons "456" (cons "789" empty)))))
 
 (define (numeric-strings list)
   (cond [(empty? (first list)) list]
         [(string-numeric? (first list)) (numeric-strings (cons (first list) (rest list)))]
         [else (cons (rest list))]))
-        
